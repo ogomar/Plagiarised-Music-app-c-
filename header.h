7b7ba14dccd1f5e878035928068db93343b7d542
ogomar <75601542+ogomar@users.noreply.github.com>
2020-12-20 23:01:29 -0400

    Add files via upload

---
+++ header.h
@@ -0,0 +1,23 @@
+#ifndef header_hpp
+#define header_hpp
+
+using namespace std;
+
+typedef struct header_file
+{
+    char chunk_id[4];
+    int chunk_size;
+    char format[4];
+    char subchunk1_id[4];
+    int subchunk1_size;
+    short int audio_format;
+    short int num_channels;
+    int sample_rate;            // sample_rate denotes the sampling rate.
+    int byte_rate;
+    short int block_align;
+    short int bits_per_sample;
+    char subchunk2_id[4];
+    int subchunk2_size;            // subchunk2_size denotes the number of samples.
+} header;
+
+#endif /* waveheader_hpp */
